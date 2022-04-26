a21e8cc47769c662a709d83e90be0383066449c1
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-26 18:52:35 +0900

    activehashの導入、該当クラスの作成

---
+++ app/models/category.rb
@@ -0,0 +1,13 @@
+class Category < ActiveHash::Base
+
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'イヤホン' },
+    { id: 3, name: 'ヘッドホン' },
+    { id: 4, name: 'スピーカー' },
+    { id: 5, name: 'プレイヤー' },
+    { id: 6, name: 'アクセサリ' },
+    { id: 7, name: 'その他' }
+  ]
+  
+  end
