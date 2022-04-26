a3e8c9b83979c5a59768ca2064ece8e20c9abbdd
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-26 18:20:29 +0900

    正しい情報を入力すると投稿が保存される機能の実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,5 +2,25 @@ class ReviewsController < ApplicationController
 
   def index
   end
-  
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:item, :review_text, :price_range_id, :category_id, :evaluation_id, :user_id, :image).merge(
+      user_id: current_user.id)
+  end
+
 end
