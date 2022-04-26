90586e93a92d9b563d1ca265dd342a26cb410a4b
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-26 12:59:12 +0900

    Userモデルのバリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_format_of :birthday, with: /\A\d{4}-\d{2}-\d{2}\z/
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'must be alphanumeric characters'
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
+
 end
