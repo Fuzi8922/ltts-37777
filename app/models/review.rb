1b8c79f50f09e5268549d6389d261291378d8c79
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-26 18:24:36 +0900

    Reviewモデルのバリデーションを記述

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,13 @@ class Review < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  with_options presence: true do
+    validates :item
+    validates :image
+    validates :review_text
+    validates :price_range_id
+    validates :category_id
+    validates :evaluation_id
+  end
+
 end
