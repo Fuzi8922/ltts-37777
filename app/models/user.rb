eede483920609ce3b5886ad7e85052f6380f7555
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-27 17:54:37 +0900

    各モデルのアソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
   has_many :reviews
   has_many :comments
+  has_many :goods
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
