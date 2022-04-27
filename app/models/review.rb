9ac155f247d9679cf96b2d621d30cc7eb8e867a7
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-27 10:50:59 +0900

    アソシエーションの追記

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,11 @@ class Review < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :price_range
+  belongs_to :evaluation
+
   with_options presence: true do
     validates :item
     validates :image
