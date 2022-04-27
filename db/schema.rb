0e312512fb2e7d4aa4d42cbe5d1a766e6707d1eb
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-27 17:40:25 +0900

    Goodモデルの生成、マイグレーションの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_27_074512) do
+ActiveRecord::Schema.define(version: 2022_04_27_083756) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,15 @@ ActiveRecord::Schema.define(version: 2022_04_27_074512) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "goods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "review_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["review_id"], name: "index_goods_on_review_id"
+    t.index ["user_id"], name: "index_goods_on_user_id"
+  end
+
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "item", null: false
     t.text "review_text", null: false
@@ -73,5 +82,7 @@ ActiveRecord::Schema.define(version: 2022_04_27_074512) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "reviews"
   add_foreign_key "comments", "users"
+  add_foreign_key "goods", "reviews"
+  add_foreign_key "goods", "users"
   add_foreign_key "reviews", "users"
 end
