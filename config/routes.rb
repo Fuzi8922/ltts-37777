da1aa5063ad2401b45cdceed622e5b58b6db6ceb
Fuzi8922 <voyage1969to70@gmail.com>
2022-05-03 12:36:03 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   root to: "reviews#index"
 
   resources :reviews do
-    resources :comments, only: [:create, :destroy]
-    resources :goods,    only: [:create, :destroy]
+    resources :comments,  only: [:create, :destroy]
+    resources :goods,     only: [:create, :destroy]
+    resources :bookmarks, only: [:create, :destroy]
   end
 
 end
