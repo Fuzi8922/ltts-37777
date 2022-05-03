cb051311ca71fc6e79b6d22f72296946497d97eb
Fuzi8922 <voyage1969to70@gmail.com>
2022-05-03 16:02:28 +0900

    パラメーターで受け取れる値を修正、ユーザー情報の編集をするためのアクションを記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, :edit_user_parameters, if: :devise_controller?
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday])
+  end
+
+  def edit_user_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :birthday, :profile_text, :image])
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,29 @@
 class UsersController < ApplicationController
 
+  before_action :user_identify, only: [:edit,  :update, :show]
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
   def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :nickname, :email, :image, :profile_text)
+  end
+
+  def user_identify
+    @user = User.find(params[:id])
   end
 
 end
